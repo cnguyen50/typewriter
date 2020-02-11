51d2e2ff46707e3e820b11dcee8501c005e755e3
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-11 20:05:32 +0000

    incremented with 50ms

--- typewriter.js
+++ typewriter.js
@@ -1,15 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 
-const typewriter = function() {
-  let sentenceArray = sentence.split('');
-  for (let i = 0; i < sentence.length; i++) {
+const typewriter = function(sentence) {
+  let timer = 0;
+  for (const char of sentence) {
     setTimeout(() => {
-      process.stdout.write(sentenceArray[i]);
-      if (i === sentence.length - 1) {
-        process.stdout.write('\n');
-      }
-    }, 50 * i);
+      process.stdout.write(char);
+    }, timer)
+    timer += 50;
   }
-};
-
-typewriter();
+}
+typewriter(sentence);
